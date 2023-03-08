38ad4e2671507458246dd50d9526da95c7413902
YYDA_HP16 <yayue@ucsd.edu>
2023-03-08 09:19:07 -0800

    revised dialog in index.html

---
+++ changelog.md
@@ -0,0 +1,17 @@
+
+
+
+Resolve branching problem commands:
+
+git checkout -b HW4
+git push --set-upstream origin HW4
+git push --help
+git pull --rebase
+git branch --set-upstream-to=HW4 HW4
+git branch --set-upstream-to=origin/HW4 HW4
+ls
+git diff
+git pull
+ls
+git add .
+git commit -m "HW4 part1 part2"
--- js/customdialog.js
+++ js/customdialog.js
@@ -2,15 +2,22 @@ const likeBtn = document.getElementById("button_like");
 const dialog_favPartDialog = document.getElementById("dialog_favPartDialog");
 const output_favPartDialog = document.getElementById("output_favPartDialog");
 const confirmBtn_favPartDialog = document.getElementById("confirmBtn_favPartDialog");
+const form_favPartDialog = document.getElementById("form_favPartDialog");
+const select_favPartDialog = document.getElementById("select_favPartDialog");
 
-likeBtn.addEventListener("click", ()=>{
+
+likeBtn.addEventListener('click', ()=>{
     dialog_favPartDialog.showModal();
 });
 
-dialog_favPartDialog.addEventListener("close", ()=> {
-    output_favPartDialog.value = "Thank you!";
+select_favPartDialog.addEventListener('change', ()=> {
+    confirmBtn_favPartDialog.value = select_favPartDialog.value;
+});
 
-
+dialog_favPartDialog.addEventListener('close', ()=> {
+    // output_favPartDialog.innerText = "Thank you!"+${dialog_favPartDialog.returnValue};
+    // output_favPartDialog.innerHTML= `Thank you! ${dialog_favPartDialog.returnValue}`;
+    output_favPartDialog.value= `Thank you! ${dialog_favPartDialog.returnValue}`;
     // returnValue_favDialog_purified = DOMPurify.sanitize(favDialog.returnValue); 
     // output_favPartDialog.value = 'ReturnValue: ${returnValue_favDialog_purified}.';
 });
