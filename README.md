37955e65a3efc90183a698c12dc68e82fee4cb49
YYDA_HP16 <yayue@ucsd.edu>
2023-03-08 09:24:41 -0800

    revised README.txt

--- README.md
+++ README.md
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 # CSE134B_HW4
-Yang Yue
-=======
-# CSE134B_HW3
+
 Name: Yang Yue
->>>>>>> e13d6da8c8214e135d78358f3bff4b7a3837a9e0
 
-
-<<<<<<< HEAD
 Link: https://thunderous-bubblegum-dfb55d.netlify.app
-=======
-PID: A16147490
 
-Link: https://yangyue.netlify.app
->>>>>>> e13d6da8c8214e135d78358f3bff4b7a3837a9e0
+
--- js/customdialog.js
+++ js/customdialog.js
@@ -1,3 +1,6 @@
+// refers to https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dialog
+
+
 const likeBtn = document.getElementById("button_like");
 const dialog_favPartDialog = document.getElementById("dialog_favPartDialog");
 const output_favPartDialog = document.getElementById("output_favPartDialog");
@@ -6,10 +9,13 @@ const form_favPartDialog = document.getElementById("form_favPartDialog");
 const select_favPartDialog = document.getElementById("select_favPartDialog");
 
 
+
 likeBtn.addEventListener('click', ()=>{
     dialog_favPartDialog.showModal();
 });
 
+// The guide gives (e) \\\\\\\\\\\\\\\\\\ WHY
+// select_favPartDialog.addEventListener('change', (e)=> {
 select_favPartDialog.addEventListener('change', ()=> {
     confirmBtn_favPartDialog.value = select_favPartDialog.value;
 });
